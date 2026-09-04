b43d6e8ff2f508eb05e6c0484f6eed46f9f55309
Sebastian Luis Enrique Aguilar Díaz <sebastian.luis.aguilar.diaz@gmail.com>
2026-09-04 13:48:00 -0600

    Prime factors done

--- Portfolio/05-JS/js/prime.js
+++ Portfolio/05-JS/js/prime.js
@@ -1,8 +1,7 @@
 /*
-    Prime Factorization - Have the user enter a number and find
-    all Prime Factors (if there are any) and display them.
+Prime Factorization - Have the user enter a number and find
+all Prime Factors (if there are any) and display them.
 */
-
 var getPrimeFactors = function (n) {
   "use strict";
 
@@ -19,12 +18,27 @@ var getPrimeFactors = function (n) {
 
   var i,
     sequence = [];
-
   //TODO: Check which numbers are factors of n and also check if
   // that number also happens to be a prime
-
+  if (isPrime(n)) {
+    sequence.push(n);
+  } else {
+    while (!isPrime(n)) {
+      for (i = 2; i * i < n; i++) {
+        if (!isPrime(i)) continue;
+        while (n % i === 0) {
+          sequence.push(i);
+          n /= i;
+        }
+      }
+    }
+    if (n != 1) sequence.push(n);
+  }
+  const primeFactorsOutput = document.getElementById("pf");
+  if (primeFactorsOutput === null) console.log("undefined");
+  else primeFactorsOutput.textContent = sequence;
   return sequence;
 };
 
 // the prime factors for this number are: [ 2, 3, 5, 7, 11, 13 ]
-console.log(getPrimeFactors(30030));
+// console.log(getPrimeFactors(30030));
